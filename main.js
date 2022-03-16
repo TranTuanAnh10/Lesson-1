55fcc8da6563419745fbbbae295d228e19667502
TranTuanAnh10 <101682072+TranTuanAnh10@users.noreply.github.com>
2022-03-16 14:12:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+var abc = "aaaaa";
+//    Built-in
+// console.log(abc);
+// confirm('xac nhan du tuoi');
+// prompt(abc);
+// setTimeout(function(){
+//     alert(abc);
+// },3000)
+// setInterval(function(){
+//        console.log('day la log' + Math.random)
+//      },2000)
+
+//   toan tu
+
+ //var a=3; var b=3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// if(a==b){
+//     console.log('a = b');
+// }
+// else{
+//     console.log('a != b');
+// }
+
+// if(a>0 && b>0){
+//     console.log("true");
+// }
+
+//Toan tu so hoc
+// var a=1; var b=3;
+// var c = a+b;
+// console.log(c)
+
+// toan tu gan
+// var a=4; var b=2;
+// a += b;
+// console.log(a)
+// a -= b;console.log(a)
+// a *= b;console.log(a)
+// a /= b;console.log(a)
+// a **= b;console.log(a)
+
+// toan tu chuoi
+// var firstName = 'Tuan'; var lastName = 'Anh'
+// console.log(firstName + ' ' + lastName)
+// firstName += ' Anh';
+// console.log(firstName)
+
+//kieu du lieu 
+
+// function tonghaiso(a,b) {
+//     if(a!==0){
+//         var x = -b/a;
+//         return x;
+//     }
+//     else{
+//         x = 'vo nghiem';
+//         return x;
+//     }
+// }
+// console.log(tonghaiso(0,6))
+
+// function writeLog(){
+//     var myString = '';
+//     for(var param of arguments){
+//         myString += `${param} - `
+//     }console.log(myString);
+// }
+// writeLog('a','b','c',1,5,null)
+
+// var lenguages = ['a','b','c'];
+// console.log(lenguages[1])
+
+// function countDown(num){
+//     if(num>0){
+//       console.log(num);
+//         return countDown(num - 1)
+//     }
+//     return num
+// }
+// countDown(1);
